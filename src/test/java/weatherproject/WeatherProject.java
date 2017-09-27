70e0fe5554455bf42af0337af08cbe9540b4427c
mimalk <mimalk@ttu.ee>
2017-09-27 16:20:16 +0300

    Test1

---
+++ src/test/java/weatherproject/WeatherProject.java
@@ -0,0 +1,35 @@
+package weatherproject;
+
+import org.junit.Test;
+
+
+/**
+ * Tests for the weather APImm.
+ */
+public class WeatherProject {
+    @Test
+    public void testCurrentTemperature(){
+    }
+
+    @Test
+    public void testCoordinates() {
+
+    }
+    @Test
+    public void testHighestTemperature() {
+
+    }
+    @Test
+    public void testLowestTemperature() {
+
+    }
+    @Test
+    public void testHTTPConnection() {
+
+    }
+    @Test
+    public void testForecastReleaseFrequency() {
+
+    }
+
+}
