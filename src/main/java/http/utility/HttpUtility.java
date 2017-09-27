303ed602854fd44c8328c128bc28924a9b0c571d
mimalk <mimalk@ttu.ee>
2017-09-27 17:30:12 +0300

    Test2

---
+++ src/main/java/http/utility/HttpUtility.java
@@ -0,0 +1,17 @@
+package http.utility;
+
+import java.net.HttpURLConnection;
+
+/**
+ * HTTP utility class.
+ */
+public class HttpUtility {
+
+    public static String createWeatherAPIURL() {
+        return null;
+    }
+
+    public static HttpURLConnection makeUrlConnection(String url) {
+        return null;
+    }
+}
---
+++ src/test/java/weatherproject/WeatherProjectTest.java
@@ -0,0 +1,50 @@
+package weatherproject;
+
+import http.utility.HttpUtility;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Tests for the weather API.
+ */
+public class WeatherProjectTest {
+    private static int HTTP_CODE_SUCCESS = 200;
+    @Test
+    public void testCurrentTemperature(){
+    }
+
+    @Test
+    public void testCoordinates() {
+
+
+    }
+    @Test
+    public void testForecastHighestTemperature() {
+
+    }
+    @Test
+    public void testForecastLowestTemperature() {
+
+    }
+    @Test
+    public void testWeatherAPIHTTPConnection() {
+        try {
+            String weatherAPIURL = HttpUtility.createWeatherAPIURL();
+            HttpURLConnection connection = HttpUtility.makeUrlConnection(weatherAPIURL);
+            assertEquals(connection.getResponseCode(), HTTP_CODE_SUCCESS);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Test
+    public void testRequestCityEqualsResponseCity() {
+
+    }
+
+}
